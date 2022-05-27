1d07c4c88ae31c4ae08f10a162c7ac0d5408b50c
Ilya Buziuk <ibuziuk@redhat.com>
2022-05-27 11:14:32 +0200

    feat: Adding basic database configuration
    Signed-off-by: Ilya Buziuk <ibuziuk@redhat.com>

--- src/main/java/io/developer/sandbox/CheDatabaseCleaner.java
+++ src/main/java/io/developer/sandbox/CheDatabaseCleaner.java
@@ -12,19 +12,47 @@
 package io.developer.sandbox;
 
 import javax.ws.rs.Produces;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.inject.Inject;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+
+
+import io.agroal.api.AgroalDataSource;
+
 import javax.ws.rs.core.MediaType;
 
 
 @Path("")
 public class CheDatabaseCleaner {
 	
+	@Inject
+	AgroalDataSource dataSource;
+	
 	@Path("{id}")
 	@Produces(MediaType.TEXT_PLAIN)
 	@DELETE
 	public Response deleteUserData(String id) {
+		
+		try (Connection connection = dataSource.getConnection()) {
+			System.out.println("Connection has been created: " + connection);
+			try (Statement statement = connection.createStatement()) {
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM usr");
+				resultSet.next();
+				System.out.println("Result set:" + resultSet);
+			} catch (SQLException e) {
+                System.out.println("Error processing statement: " + e);
+            }
+		} catch (SQLException e) {
+			System.out.println("Error processing connection: " + e);
+		}
+		
 		return Response.ok(id, MediaType.TEXT_PLAIN).build();
 	}
 
